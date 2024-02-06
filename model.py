f1b685529eae7652776848c0a7ef44e16a0ff865
Dabyz24 <david.lopez.pereira@gmail.com>
2024-02-06 13:03:55 +0100

    Creacion boceto de modelo

--- model.py
+++ model.py
@@ -1,37 +1,64 @@
-import pygame as pg 
-from random import randrange
-from agents import Ant
-from fabrics import Planets
-import time
+import mesa 
 
-NUMTECHPLANETS = 3
-NUMRICHPLANETS = 3
+from agents import Player, Planets
 
-class Juego:
-    def __init__(self, WIDTH=1600, HEIGHT=900, CELL_SIZE=10):
-        pg.init()
-        self.screen = pg.display.set_mode([WIDTH, HEIGHT]) # Crea una pantalla de 1600 por 900
-        self.clock = pg.time.Clock()  # Establece el numero de frames de screen 
-        self.CELL_SIZE = CELL_SIZE
-        self.ROWS, self.COLS = HEIGHT // CELL_SIZE, WIDTH // CELL_SIZE  # 900/10 serán las filas y 1600/10 serán las columnas
-        self.grid = [[0 for _ in range(self.COLS)]for _ in range(self.ROWS)]  # EL tablero será una matriz con el numero de columnas y filas inicializado a 0 
-        self.ants = [Ant(self, [2, 45], (128,128,128)) for _ in range(1)]
-        # Pensar una manera más sofisticada para crear de una manera equilibrada planetas con más tecnología y otros con más dinero, los que tengan más dinero tendrán más impuestos
-        techPlanets = [Planets(self, pos=[randrange(200, WIDTH), randrange(100, HEIGHT)], length=randrange(10,30), tech=randrange(30,100), money=randrange(20,30), taxes=randrange(10,30))for _ in range(NUMTECHPLANETS)]
-        richPlanets = [Planets(self, pos=[randrange(200, WIDTH), randrange(100, HEIGHT)], length=randrange(10,30), tech=randrange(0,20), money=randrange(20,200), taxes=randrange(10,50))for _ in range(NUMRICHPLANETS)]
-        self.planets = techPlanets + richPlanets
-        for planet in self.planets:
-            planet.draw()
 
-    def run(self):
-        while True:
-            [ant.run() for ant in self.ants]
-            # # Obtener las coordenadas del ratón
-            # mouse_x, mouse_y = pg.mouse.get_pos()
-            # print(f"{mouse_x}, {mouse_y}")
-            for event in pg.event.get():
-                if event.type == pg.QUIT:
-                    pg.quit()
-            time.sleep(0.5)
-            pg.display.flip()
-            self.clock.tick()
+class Game(mesa.model):
+
+    def __init__(self, width=20, height=20, num_players=2, num_planets=5, prob_factory=0.4, prob_weapon=0.1, prob_space_ship=0.6, tech_planet=20, gold_planet=30, taxes_planet=20):
+        """
+        width: Ancho de la matriz donde se encontraran los agentes
+        height: Alto de la matriz donde se encontraran los agentes 
+        num_players: Se trata del numero de jugadores que queremos en la partida
+        num_planets: Se trata del numero de planetas que querremos en la partida
+        prob_factory: Probabilidad de construir una fabrica en el turno
+        prob_weapon: Probabilidad de construir un arma en el turno
+        prob_space_ship: Probabilidad de construir una nave espacial para la exploracion en el turno
+        tech_planet: Tecnología que tendrá cada planeta
+        gold_planet: Oro que tendrá cada planeta
+        taxes_planet: Impuesto que deberán pagar por cada planeta
+        """
+        super.__init__()
+        self.width = width
+        self.height = height
+        self.num_players = num_players
+        self.num_planets = num_planets
+        self.prob_factory = prob_factory
+        self.prob_weapon = prob_weapon 
+        self.prob_space_ship = prob_space_ship
+        self.tech_planet = tech_planet
+        self.gold_planet = gold_planet
+        self.taxes_planet = taxes_planet
+        # Se moverán uno cada vez, es decir el primer turno se movera primero el agente 1 y el siguiente el agente 2 primero
+        self.schedule = mesa.time.RandomActivation(self)
+        # Creacion de la matriz Torus=True significa que si el agente se encuentra en la izquierda del todo y sigue a la izquierda aparecera en la derecha 
+        self.grid = mesa.space.MultiGrid(self.width, self.height, torus=True)
+        # Datos que queremos ver 
+        self.datacollector = mesa.DataCollector(
+            {
+                "Number of players": self.num_players,
+                "Taxes for Planet": self.taxes_planet
+            }
+        )
+        
+        # Creacion de los jugadores
+        for _ in range(self.num_players):
+            pos = (self.random.randrange(self.width), self.random.randrange(self.height))
+            player = Player(self.next_id(), self, pos)
+            self.grid.place_agent(player, pos)
+            self.schedule.add(player)
+        
+        # Creacion de los planetas 
+        for _ in range(self.num_planets):
+            pos = (self.random.randrange(self.width), self.random.randrange(self.height))
+            planet = Planets(self.next_id(), self, pos, self.tech_planet, self.gold_planet, self.taxes_planet)
+            self.grid.place_agent(planet, pos)
+            self.schedule.add(planet)
+
+    def step(self):
+        self.schedule.step()
+        self.datacollector.collect(self)
+    
+    def run_model(self, n):
+        for i in range(n):
+            self.step()
