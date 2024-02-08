bb6de5222a680be5305826fc2b457465eaba8e12
Dabyz24 <david.lopez.pereira@gmail.com>
2024-02-08 13:18:20 +0100

    Boceto de como pueden ser los steps

--- agents.py
+++ agents.py
@@ -1,7 +1,13 @@
 import mesa
 
+INITIAL_PLAYER_GOLD = 100
+INITIAL_PLAYER_TECH = 30
+GOLD_FROM_FACTORIES = 20
+TECH_FROM_FACTORIES = 5
+
+
 class Player(mesa.Agent):
-    def __init__(self, unique_id, model, pos, tech=30, gold=100, num_planets=1 ,estelar_points=0, moore=True):
+    def __init__(self, unique_id, model, pos, tech=INITIAL_PLAYER_TECH, gold=INITIAL_PLAYER_GOLD, num_planets=0, num_factories=0 ,estelar_points=0, moore=True):
         """
         unique_id: Servira para identificar al agente por un id unico
         model: Se trata del modelo donde habitara el agente
@@ -9,6 +15,7 @@ class Player(mesa.Agent):
         tech: Valor de tecnología que tendrá el agente de manera inicial será 30
         gold: Valor de oro que tendrá el agente de manera inicial será 100
         num_planets: Número de planetas conquistados
+        num_factories: Número de fabricas que ha creado cada agente
         estelar_points: El valor más importante del juego, decidirá quien es el ganador
         moore: Si es True el agente podrá moverse en las 8 direcciones, si no solo podrá arriba, abajo, derecha e izquierda
         """
@@ -17,10 +24,13 @@ class Player(mesa.Agent):
         self.tech = tech
         self.gold = gold
         self.num_planets = num_planets
+        self.num_factories = num_factories
         self.estelar_points = estelar_points
         self.moore = moore
+        # Permite saber si se ha creado una nave para permitir moverse o no, inicialmente será False
+        self.move = False
     
-    def addResources(self, tech, gold):
+    def addPlanetResources(self, tech, gold):
         self.tech += tech
         self.gold += gold
         self.num_planets += 1
@@ -28,6 +38,20 @@ class Player(mesa.Agent):
     def getResources(self):
         return "Tech: " + str(self.tech) + " Gold: " + str(self.gold)
     
+    def createFactory(self):
+        self.num_factories += 1
+
+    def addFactoryResources(self):
+        self.tech += TECH_FROM_FACTORIES
+        self.gold += GOLD_FROM_FACTORIES
+
+    def enoughResources(self, tech, gold):
+        if self.gold >= gold and self.tech >= tech:
+            self.tech -= tech
+            self.gold -= gold
+            return True
+        return False
+             
     def payTaxes(self, taxes=20):
         taxes = taxes * self.num_planets
         self.gold -= taxes
@@ -37,6 +61,32 @@ class Player(mesa.Agent):
         El step representará cada turno del juego. Podrá decidir si moverse, construir o atacar 
         """
         # Tengo que comprobar si tiene alguna fabrica generada o alguna nave para poder moverse
+        options = ["Factory", "Space_ship", "Weapon"]
+        probabilities = [self.model.prob_factory, self.model.prob_space_ship, self.model.prob_weapon]
+        choose_action = self.random.choices(options, weights=probabilities, k=1)[0]
+
+        if choose_action == "Factory" and self.enoughResources(5, 30):
+            print("Fabrica creada")
+            self.createFactory
+            
+
+        if choose_action == "Space_ship":
+            print("Space_ship")
+            # Determinara si ya ha creado una nave espacial para moverse antes o no 
+            if self.move: 
+                next_moves = self.model.grid.get_neighborhood(self.pos, self.moore, include_center=False)
+                next_move = self.random.choice(next_moves)                    
+                self.model.grid.move_agent(self, next_move)
+            
+            elif self.enoughResources(15, 20):
+                self.move = True
+
+        if choose_action == "Weapon" and self.enoughResources(20, 40):
+            print("Arma creada")
+
+
+        if self.num_factories > 0:
+            self.addFactoryResources()
         self.payTaxes()
 
 
@@ -77,6 +127,6 @@ class Planet(mesa.Agent):
                 player_selected = self.random.choice(list_players)
                 self.populated = True
                 # pasa a ser habitado por el agente seleccionado
-                player_selected.addResources(self.tech, self.gold)
+                player_selected.addPlanetResources(self.tech, self.gold)
         
 
