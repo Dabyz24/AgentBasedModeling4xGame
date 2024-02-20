74918eaa1b44d11955eae4e053a4df82ab4269ef
Dabyz24 <david.lopez.pereira@gmail.com>
2024-02-20 13:18:16 +0100

    Prueba de como serían las armas

---
+++ weapons.py
@@ -0,0 +1,11 @@
+
+class Weapon():
+    
+    def __init__(self):
+        self.list_weapons = ["Lasers", "Plasma Cannon", "Guided missil"]
+        self.value_weapons = {
+            "Lasers": 2,
+            "Plasma Cannon": 3,
+            "Guided missil" : 5
+        }
+
