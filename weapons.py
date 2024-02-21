51580bc276437e38ce61eebdba02ea818d9fe6c0
Dabyz24 <david.lopez.pereira@gmail.com>
2024-02-21 17:39:39 +0100

    Creacion de la clase que se encargara de manejar las armas de los agentes

--- weapons.py
+++ weapons.py
@@ -2,10 +2,40 @@
 class Weapon():
     
     def __init__(self):
+        """
+        actual_weapon: Arma actual del agente, empezará sin arma
+        num_upgrades: Numero de mejoras que se han hecho al arma como maximo se podrán 3
+        list_weapons: lista de armas del juego
+        value_weapons: Diccionario con el daño asociado a cada arma
+        """
+        self.actual_weapon = ""
+        self.num_upgrades = 0
         self.list_weapons = ["Lasers", "Plasma Cannon", "Guided missil"]
         self.value_weapons = {
             "Lasers": 2,
             "Plasma Cannon": 3,
             "Guided missil" : 5
         }
+        
 
+    def upgradeWeapon(self):
+        try:
+            # Si el arma actual se encuentra entre las armas del juego, miramos la siguiente arma en la lista y asignamos su valor a actual_weapon 
+            index = self.list_weapons.index(self.actual_weapon)
+            # Podremos incrementar el arma porque el indice es menor a la longitud de la lista 
+            if index + 1 < len(self.list_weapons):
+                self.actual_weapon = self.list_weapons[index+1]
+            self.num_upgrades += 1
+        except:
+            # Si no tiene arma creamos una
+            self.actual_weapon = self.list_weapons[0]
+            self.num_upgrades += 1
+
+
+    def getWeapon(self):
+        if self.actual_weapon == "":
+            return "None"
+        return self.actual_weapon, self.value_weapons[self.actual_weapon]
+    
+    def getNumUpgrades(self):
+        return self.num_upgrades
