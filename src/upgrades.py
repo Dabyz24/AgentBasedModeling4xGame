fd0a292386372d3219ce95656dd31d2c00179697
Dabyz24 <david.lopez.pereira@gmail.com>
2024-03-20 22:13:43 +0100

    quitado el upgrade de movimiento y alñadido un comentario para añadir mas upgrades en un futuro

--- src/upgrades.py
+++ src/upgrades.py
@@ -4,20 +4,21 @@ class Upgrades():
     def __init__(self):
         """
         Clase que sirve para tener un arbol de tecnologías para poder mejorar atributos de los agentes 
+        Si se quieren añadir más upgrades simplemente sería poner una nueva variable booleana y añadirla a la funcion 
+        para luego en la clase agente mediante esa variable modificar el funcionamiento deseado
 
         Atributos:
 
             damage_upgrade: Permite mejorar la probabilidad a la hora de ganar los combates 
-            movement_upgrade: Permite moverse dos posiciones en un turno
             factories_upgrades: Permite duplicar las ganancias de las fabricas
             list_upgrades: Tendrá guardadas todos las mejoras para poder obtener el numero maximo de upgrades permitidos
             num_upgrades: Llevará la cuenta de las mejoras para saber si se pueden mejorar más o ya ha alcanzado el límite
         """
         self.damage_upgrade = False
-        self.movement_upgrade = False
         self.factories_upgrade = False
-        self.list_upgrades = ["Damage","Movement","Factory"]
+        self.list_upgrades = ["Damage","Factory"]
         self.num_upgrades = 0
+
 
     def upgradeDamage(self):
         if not self.damage_upgrade:
@@ -25,12 +26,6 @@ class Upgrades():
             self.num_upgrades += 1
             self.list_upgrades.remove("Damage")
 
-    def upgradeMovement(self):
-        if not self.movement_upgrade:
-            self.movement_upgrade = True
-            self.num_upgrades += 1
-            self.list_upgrades.remove("Movement")
-    
     def upgradeFactories(self):
         if not self.factories_upgrade:
             self.factories_upgrade = True
@@ -41,9 +36,6 @@ class Upgrades():
     def isDamageUpgraded(self):
         return self.damage_upgrade
     
-    def isMovementUpgraded(self):
-        return self.movement_upgrade
-
     def getNumUpgrades(self):
         return self.num_upgrades
     
@@ -58,8 +50,6 @@ class Upgrades():
         aux_str = ""
         if self.damage_upgrade:
             aux_str += "D ↑ "
-        if self.movement_upgrade:
-            aux_str += "M ↑ "
         if self.factories_upgrade:
             aux_str += "F ↑ "
         if aux_str == "": return "None"
