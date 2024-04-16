e2dfd077ac691536fcf8326602ed3a2845121208
Dabyz24 <david.lopez.pereira@gmail.com>
2024-04-16 16:40:47 +0200

    Cambiado el orden de lista POSSIBLE_BEHAVIOURS

--- src/global_constants.py
+++ src/global_constants.py
@@ -51,4 +51,4 @@ Las siglas se corresponden con:
 POSSIBLE_ACTIONS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 # Conjunto de comportamientos posibles en el agente
-POSSIBLE_BEHAVIOURS = ["Chaser", "Explorer", "Farmer"]
+POSSIBLE_BEHAVIOURS = ["Explorer", "Chaser", "Farmer"]
