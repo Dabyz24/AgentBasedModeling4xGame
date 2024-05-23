93951b471c8860f4e1d1b771170ed891393d5441
Dabyz24 <david.lopez.pereira@gmail.com>
2024-05-23 16:57:34 +0200

    Polimorfismo en las clases del comportamiento

--- src/behaviours.py
+++ src/behaviours.py
@@ -17,79 +17,45 @@ class Behaviour():
     def __init__(self):
         # Nombre del comportamiento
         self.actual_behaviour = ""
-        self.initial_behaviours = ["Explorer", "Chaser", "Farmer"]
         # Diccionario con las acciones posibles y direccion de movimiento
         self.dict_actions = {"Move": {"To_Planet": False, "To_Player":False}, "Factory": 0, "Weapon": 0, "Upgrade": {"Damage": False, "Factory":False}}
         # Lista que servirá para tener las prioridades
         self.list_priorities = []
-        # Servira para comprobar que al establecer una nueva prioridad se pongan los numeros correctos
-        self._valid_numbers_priority = ["1","2","3","4"]
+        self.setBehaviourName("Random")
+        # Inicializaré una clase de comportamiento con todo aleatorio
+        self.setRandomPriorities()
     
-    # Metodo para establecer las prioridades de los comportamientos creados
-    def setPriorities(self, behaviour, random_flag=False):
-        # Si se trata de un comportamiento preestablecido se crearan las prioridades y las acciones especiales directamente
-        if behaviour in self.initial_behaviours:
-            if behaviour == "Explorer":
-                self.dict_actions["Move"]["To_Planet"] = True
-                self.list_priorities = ["Move", "Factory", "Upgrade", "Weapon"]
-            if behaviour == "Chaser":
-                self.dict_actions["Move"]["To_Player"] = True
-                self.dict_actions["Upgrade"]["Damage"] = True
-                self.list_priorities = ["Weapon", "Upgrade", "Move", "Factory"]
-            if behaviour == "Farmer":
-                self.dict_actions["Upgrade"]["Factory"] = True
-                self.list_priorities = ["Factory", "Upgrade", "Move", "Weapon"]
-        # Si la flag random está en True se establecerán las prioridades de manera aleatoria 
-        elif random_flag:
-            list_actions = list(self.dict_actions.keys())
-            random.shuffle(list_actions)
-            for action in list_actions:
-                if action == "Move":
-                    self.getRandomSpecialActions(action)
-                elif action == "Upgrade":
-                    self.getRandomSpecialActions(action)
-                self.list_priorities.append(action)
-        else:
-            # Si el nombre no está en la lista de preestablecidos se preguntará la direccion del movimiento y las mejoras disponibles
-            different_priorities = self.inputPriorities()
-            str_special_moves = input("Type the letter if you want to chase an Agent (A) a Planet (P) or None (N). ").upper()
-            if str_special_moves == "A":
-                self.dict_actions["Move"]["To_Player"] = True
-            elif str_special_moves == "P":
-                self.dict_actions["Move"]["To_Planet"] = True
-            else:
-                print("The agent will do random moves")
-            
-            str_special_upgrades = input("Type the letter if you want to upgrade Damage (D) Factories (F) both (B) or None (N). ").upper()
-            if str_special_upgrades == "D":
-                self.dict_actions["Upgrade"]["Damage"] = True
-            elif str_special_upgrades == "F":
-                self.dict_actions["Upgrade"]["Factory"] = True
-            elif str_special_upgrades == "B":
-                self.dict_actions["Upgrade"]["Damage"] = True
-                self.dict_actions["Upgrade"]["Factory"] = True
-            else:
-                print("No upgrades for your agent")
-            # Se crea una lista para poder acceder de manera más simple a las claves del diccionario
-            list_actions = list(self.dict_actions.keys())
-            for priority in different_priorities:
-                # Se resta uno en la priority para que coincida con el indice de dict_actions
-                self.list_priorities.append(list_actions[int(priority)-1])
+    def act(self):
+        # Metodo que determinará el comportamiento de la clase Behaviour
+        pass
 
-    def inputPriorities(self):
-        # Mientras no cumpla la condicion para establecer la lista de prioridades se repetirá el input 
-        while True:  
-                str_priority = input("Select from highest to lowest priority (1 = Move, 2 = Factory, 3 = Weapon, 4 = Upgrade) using comma as separator. Ex: 1,2,3,4\n")
-                different_priorities = str_priority.split(",")
-                # Si todos los numeros introducidos estan dentro de la lista de numeros validos 
-                if all(number in self._valid_numbers_priority for number in different_priorities) and len(different_priorities) == len(self.dict_actions):
-                        return different_priorities
+    # Metodo para establecer las prioridades aleatorias del comportamiento
+    def setRandomPriorities(self):
+        # Paso a una lista todas las claves del diccionario de acciones para poder recorrerla
+        list_actions = list(self.dict_actions.keys())
+        # Los ordeno de forma aleatoria para tener una lista aleatoria con los comportamientos
+        random.shuffle(list_actions)
+        # Recorro la lista para determinar una direccion al movimiento y las mejoras que tendrá
+        for action in list_actions:
+            if action == "Move":
+                self.getRandomSpecialActions(action)
+            elif action == "Upgrade":
+                self.getRandomSpecialActions(action)
+            # Por último guardo la lista de prioridad en la variable correspondiente
+            self.list_priorities.append(action)
+
+    # Método para establecer de manera aleatoria la direccion del movimiento y el comportamiento
+    def getRandomSpecialActions(self, action):
+        if action == "Move" or action == "Upgrade":
+            for item in self.dict_actions[action]:
+                coin = random.randint(0,1)
+                if coin == 0:
+                    self.dict_actions[action][item] = False
                 else:
-                    print(f"This are the only valid numbers: {self._valid_numbers_priority}")
+                    self.dict_actions[action][item] = True
 
-
+    # Método para presentar de una manera mas visual la lista de prioridades
     def getPrioritiesStr(self):
-        # Método para presentar de una manera mas visual la lista de prioridades
         aux_str = ""
         for i in range(0, len(self.list_priorities)):
             if i+1 == len(self.list_priorities):
@@ -116,39 +82,35 @@ class Behaviour():
                 
         return self.list_priorities, list_special_move, list_special_upgrade
     
-    def newBehaviour(self, new, flag=False):
-        """
-        Crear un nuevo comportamiento, reseteando los atributos principales y llamando al metodo setPriorities()
-        """
-        self.list_priorities = []
-        self.resetDictActions()
-        self.actual_behaviour = new
-        if flag:
-            self.setPriorities(new, random_flag=True)
-        else:
-            self.setPriorities(new)
+    # def newBehaviour(self, new, flag=False):
+    #     """
+    #     Crear un nuevo comportamiento, reseteando los atributos principales y llamando al metodo setPriorities()
+    #     """
+    #     self.list_priorities = []
+    #     self.resetDictActions()
+    #     self.actual_behaviour = new
+    #     if flag:
+    #         self.setPriorities(new, random_flag=True)
+    #     else:
+    #         self.setPriorities(new)
 
-    def resetDictActions(self):
+    def resetBehaviour(self):
         self.dict_actions = {"Move": {"To_Planet": False, "To_Player":False}, "Factory": 0, "Weapon": 0, "Upgrade": {"Damage": False, "Factory":False}}
-
-    def getRandomSpecialActions(self, action):
-        # Método para establecer de manera aleatoria la direccion del movimiento y el comportamiento
-        if action == "Move" or action == "Upgrade":
-            for item in self.dict_actions[action]:
-                coin = random.randint(0,1)
-                if coin == 0:
-                    self.dict_actions[action][item] = False
-                else:
-                    self.dict_actions[action][item] = True
+        self.list_priorities = []
 
     def getActualBehaviour(self):
         return self.actual_behaviour
+    
+    def setBehaviourName(self, new_name):
+        self.actual_behaviour = new_name
     
 
 class Explorer(Behaviour):
 
     def __init__(self):
         super().__init__()
+        self.resetBehaviour()
+        self.setBehaviourName(self.__class__.__name__)
         self.actual_behaviour = self.__class__.__name__
         self.dict_actions["Move"]["To_Planet"] = True
         self.list_priorities = ["Move", "Factory", "Upgrade", "Weapon"]
@@ -157,24 +119,107 @@ class Explorer(Behaviour):
         # Metodo para determinar como actuarán los exploradores
         pass
 
+class Chaser(Behaviour):
+
+    def __init__(self):
+        super().__init__()
+        self.resetBehaviour()
+        self.setBehaviourName(self.__class__.__name__)
+        self.dict_actions["Move"]["To_Player"] = True
+        self.dict_actions["Upgrade"]["Damage"] = True
+        self.list_priorities = ["Weapon", "Upgrade", "Move", "Factory"]
+
+    def act():
+        # Metodo para determinar como actuarán los chasers
+        pass
+
+class Farmer(Behaviour):
+
+    def __init__(self):
+        super().__init__()
+        self.resetBehaviour()
+        self.setBehaviourName(self.__class__.__name__)
+        self.dict_actions["Upgrade"]["Factory"] = True
+        self.list_priorities = ["Factory", "Upgrade", "Move", "Weapon"]
+
+    def act():
+        # Metodo para determinar como actuarán los farmers
+        pass
+
+class CustomBehaviour(Behaviour):
+
+    def __init__(self, behaviour_name):
+        super().__init__()
+        self.setBehaviourName(behaviour_name)
+        # Servira para comprobar que al establecer una nueva prioridad se pongan los numeros correctos
+        self._valid_numbers_priority = ["1","2","3","4"]
+        self.resetBehaviour()
+        self.setPriorities()
+        
+    def setPriorities(self):
+        # Método para establecer de una manera personalizada la lista de prioridad, la direccion de movimiento y las mejoras disponibles 
+        different_priorities = self.inputPriorities()
+        str_special_moves = input("Type the letter if you want to chase an Agent (A) a Planet (P) or None (N). ").upper()
+        if str_special_moves == "A":
+            self.dict_actions["Move"]["To_Player"] = True
+        elif str_special_moves == "P":
+            self.dict_actions["Move"]["To_Planet"] = True
+        else:
+            print("The agent will do random moves")
+        
+        str_special_upgrades = input("Type the letter if you want to upgrade Damage (D) Factories (F) both (B) or None (N). ").upper()
+        if str_special_upgrades == "D":
+            self.dict_actions["Upgrade"]["Damage"] = True
+        elif str_special_upgrades == "F":
+            self.dict_actions["Upgrade"]["Factory"] = True
+        elif str_special_upgrades == "B":
+            self.dict_actions["Upgrade"]["Damage"] = True
+            self.dict_actions["Upgrade"]["Factory"] = True
+        else:
+            print("No upgrades for your agent")
+        # Se crea una lista para poder acceder de manera más simple a las claves del diccionario
+        list_actions = list(self.dict_actions.keys())
+        for priority in different_priorities:
+            # Se resta uno en la priority para que coincida con el indice de dict_actions
+            self.list_priorities.append(list_actions[int(priority)-1])
+
+    def inputPriorities(self):
+        # Mientras no cumpla la condicion para establecer la lista de prioridades se repetirá el input 
+        while True:  
+                str_priority = input("Select from highest to lowest priority (1 = Move, 2 = Factory, 3 = Weapon, 4 = Upgrade) using comma as separator. Ex: 1,2,3,4\n")
+                different_priorities = str_priority.split(",")
+                # Si todos los numeros introducidos estan dentro de la lista de numeros validos 
+                if all(number in self._valid_numbers_priority for number in different_priorities) and len(set(different_priorities)) == len(self.dict_actions):
+                        return different_priorities
+                else:
+                    print(f"This are the only valid numbers: {self._valid_numbers_priority}")
+
+    def act():
+        # Metodo para determinar como actuarán los que tengan un comportamiento personalizado 
+        pass
+
+
 # Para poder comprobar el funcionamiento de la clase 
 if __name__ == "__main__":
-    comportamiento = Explorer()
-    print(comportamiento.actual_behaviour)
+    comportamiento = Behaviour()
+    explorador = Explorer()
+    perseguidor = Chaser()
+    granjero = Farmer()
+    custom = CustomBehaviour("nombre")
+    
+    print(f"nombre de comporamiento de Clase Behaviour -> {comportamiento.actual_behaviour}")
+    print(f"nombre de comporamiento de Clase Explorer -> {explorador.actual_behaviour}")
+    print(f"nombre de comporamiento de Clase Chaser -> {perseguidor.actual_behaviour}")
+    print(f"nombre de comporamiento de Clase Farmer -> {granjero.actual_behaviour}")
+    print(f"nombre de comporamiento de Clase Personalizada -> {custom.actual_behaviour}")
+    print("Clase Behaviour")
     print(comportamiento.getPrioritiesStr())
     print(comportamiento.getPriorities())
-    comportamiento.newBehaviour("Chaser")
-    print(comportamiento.actual_behaviour)
-    print(comportamiento.getPrioritiesStr())
-    print(comportamiento.getPriorities())
-    comportamiento.newBehaviour("Farmer")
-    print(comportamiento.actual_behaviour)
-    print(comportamiento.getPrioritiesStr())
-    print(comportamiento.getPriorities())
-    comportamiento.newBehaviour("random", flag=True)
-    print(comportamiento.actual_behaviour)
-    print(comportamiento.getPrioritiesStr())
-    comportamiento.newBehaviour("new2")
-    print(comportamiento.actual_behaviour)
-    print(comportamiento.getPrioritiesStr())
+    print("Clase Explorador")
+    print(explorador.getPrioritiesStr())
+    print(explorador.getPriorities())
+    print("Clase Personalizada")
+    print(custom.getPrioritiesStr())
+    print(custom.getPriorities())
+
                
